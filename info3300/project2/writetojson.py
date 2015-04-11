6e663ab908b2b38738cec39e9e08db83912d4c6a
Joshua Cohen <jbc264@cornell.edu>
2015-04-11 19:04:31 -0400

    further cleanup of python csv to json code

--- info3300/project2/writetojson.py
+++ info3300/project2/writetojson.py
@@ -4,41 +4,51 @@ import csv
 
 def main():
     dictobject = {}
-    statelist = []
+    state_list = []
+    year_list = []
 
     for num in xrange(1990, 2011):
         dictobject[str(num)] = {}
 
-    with open('generation_annual.csv', 'rU') as generationfile:
-        generationreader = csv.reader(generationfile)
+    with open('generation_annual.csv', 'rU') as generationcsv:
+        generationreader = csv.reader(generationcsv)
         for row in generationreader:
+            if (row[0] != 'YEAR'):
+                year_list.append(row[0])
             if ((row[1] != '  ') and (row[1] != 'STATE_CODE')):
-                statelist.append(row[1])
+                state_list.append(row[1])
 
-    stateset = sorted(set(statelist))
+    year_set = sorted(set(year_list))
+    state_set = sorted(set(state_list))
 
-    for obj in dictobject:
-        for state in stateset:
-            dictobject[obj][state] = {}
+    for year in year_set:
+        dictobject[year] = {}
+        for state in state_set:
+            dictobject[year][state] = {}
 
-    with open('generation_annual.csv', 'rU') as generationfile:
-        generationreader = csv.reader(generationfile)
+    with open('generation_annual.csv', 'rU') as generationcsv:
+        generationreader = csv.reader(generationcsv)
         for row in generationreader:
             if ((row[0] != 'YEAR') and
                     (row[1] != '  ') and
                     (row[1] != 'STATE_CODE') and
                     (dictobject[row[0]][row[1]].get(row[2]) is None)):
                 dictobject[row[0]][row[1]][row[2]] = {}
+
+    with open('generation_annual.csv', 'rU') as generationcsv:
+        generationreader = csv.reader(generationcsv)
         for row in generationreader:
             if ((row[0] != 'YEAR') and
-                (row[1] != '  ') and
+                    (row[1] != '  ') and
                     (row[1] != 'STATE_CODE')):
                 dictobject[row[0]][row[1]][row[2]].update({row[3]: row[4]})
 
     with open('generation_annual.json', 'w') as generationjson:
         json.dump(dictobject, generationjson, sort_keys=True, indent=4, separators=(',', ': '))
+        generationjson.write('\n')
 
-    generationfile.close()
+    generationcsv.close()
+    generationjson.close()
 
 if __name__ == "__main__":
     main()
